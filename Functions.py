b8e7b63ee3c2172067d250eccea7a7d8e97bfaa6
ehyip123 <graceyip@live.com.sg>
2023-05-14 17:55:12 +0800

    Version 3 - Show RSI Values

--- Functions.py
+++ Functions.py
@@ -2,37 +2,36 @@
 
 import yfinance as yf
 import pandas as pd
-import streamlit as st
-import time
 
-STI_dict = {'C6L.SI':['Singapore Airlines','https://sg.finance.yahoo.com/quote/C6L.SI?p=C6L.SI&.tsrc=fin-srch'],
-                '9CI.SI':['CapitaLand Investment Limited', 'https://sg.finance.yahoo.com/quote/9CI.SI?p=9CI.SI&.tsrc=fin-srch'],
+
+STI_dict = {'C6L.SI': ['Singapore Airlines','https://sg.finance.yahoo.com/quote/C6L.SI?p=C6L.SI&.tsrc=fin-srch'],
+                '9CI.SI': ['CapitaLand Investment Limited', 'https://sg.finance.yahoo.com/quote/9CI.SI?p=9CI.SI&.tsrc=fin-srch'],
                 'BUOU.SI': ['Frasers Logistics & Commercial Trust','https://sg.finance.yahoo.com/quote/BUOU.SI?p=BUOU.SI&.tsrc=fin-srch'],
-                'ME8U.SI':['Mapletree Industrial Trust','https://finance.yahoo.com/quote/ME8U.SI?p=ME8U.SI&.tsrc=fin-srch'],
-                'C38U.SI':['CapitaLand Integrated Commercial Trust','https://finance.yahoo.com/quote/C38U.SI?p=C38U.SI&.tsrc=fin-srch'],
-                'M44U.SI':['Mapletree Logistics Trust', 'https://finance.yahoo.com/quote/M44U.SI?p=M44U.SI&.tsrc=fin-srch'],
-                'O39.SI':['OCBC', 'https://finance.yahoo.com/quote/O39.SI?p=O39.SI&.tsrc=fin-srch'],
-                'U14.SI':['UOL Group Limited', 'https://finance.yahoo.com/quote/U14.SI?p=U14.SI&.tsrc=fin-srch'],
-                'D05.SI':['DBS', 'https://finance.yahoo.com/quote/D05.SI?p=D05.SI&.tsrc=fin-srch'],
-                'A17U.SI':['CapitaLand Ascendas REIT', 'https://finance.yahoo.com/quote/A17U.SI?p=A17U.SI&.tsrc=fin-srch'],
-                'Z74.SI' :['Singtel', 'https://finance.yahoo.com/quote/Z74.SI?p=Z74.SI&.tsrc=fin-srch'],
+                'ME8U.SI': ['Mapletree Industrial Trust','https://finance.yahoo.com/quote/ME8U.SI?p=ME8U.SI&.tsrc=fin-srch'],
+                'C38U.SI': ['CapitaLand Integrated Commercial Trust','https://finance.yahoo.com/quote/C38U.SI?p=C38U.SI&.tsrc=fin-srch'],
+                'M44U.SI': ['Mapletree Logistics Trust', 'https://finance.yahoo.com/quote/M44U.SI?p=M44U.SI&.tsrc=fin-srch'],
+                'O39.SI': ['OCBC', 'https://finance.yahoo.com/quote/O39.SI?p=O39.SI&.tsrc=fin-srch'],
+                'U14.SI': ['UOL Group Limited', 'https://finance.yahoo.com/quote/U14.SI?p=U14.SI&.tsrc=fin-srch'],
+                'D05.SI': ['DBS', 'https://finance.yahoo.com/quote/D05.SI?p=D05.SI&.tsrc=fin-srch'],
+                'A17U.SI': ['CapitaLand Ascendas REIT', 'https://finance.yahoo.com/quote/A17U.SI?p=A17U.SI&.tsrc=fin-srch'],
+                'Z74.SI' : ['Singtel', 'https://finance.yahoo.com/quote/Z74.SI?p=Z74.SI&.tsrc=fin-srch'],
                 'D01.SI' : ['DFI Retail Group Holdings Limited','https://finance.yahoo.com/quote/D01.SI?p=D01.SI&.tsrc=fin-srch'],
-                'BN4.SI' :['Keppel Corporation', 'https://finance.yahoo.com/quote/BN4.SI?p=BN4.SI&.tsrc=fin-srch'],
-                'S63.SI': ['Singapore Technologies Engineering Ltd', 'https://finance.yahoo.com/quote/S63.SI?p=S63.SI&.tsrc=fin-srch'],
-                'U96.SI': ['Sembcorp', 'https://finance.yahoo.com/quote/U96.SI?p=U96.SI&.tsrc=fin-srch'],
-                'S68.SI':['SGX', 'https://finance.yahoo.com/quote/S68.SI?p=S68.SI&.tsrc=fin-srch'],
-                'V03.SI':['Venture Corp', 'https://finance.yahoo.com/quote/V03.SI?p=V03.SI&.tsrc=fin-srch'],
-                'S58.SI':['SATS', 'https://finance.yahoo.com/quote/S58.SI?p=S58.SI&.tsrc=fin-srch'],
-                'Y92.SI':['Thai Beverage', 'https://finance.yahoo.com/quote/Y92.SI?p=Y92.SI&.tsrc=fin-srch'],
-                'C07.SI':['Jardine Cycle & Carriage', 'https://finance.yahoo.com/quote/C07.SI?p=C07.SI&.tsrc=fin-srch'],
-                'C09.SI':['City Developments', 'https://finance.yahoo.com/quote/C09.SI?p=C09.SI&.tsrc=fin-srch'],
-                'N2IU.SI':['Mapletree Pan Asia Commercial Trust', 'https://finance.yahoo.com/quote/N2IU.SI?p=N2IU.SI&.tsrc=fin-srch'],
-                'H78.SI':['Hong Kong Land holdings', 'https://finance.yahoo.com/quote/H78.SI?p=H78.SI&.tsrc=fin-srch'],
-                'F34.SI':['Wilmar International', 'https://finance.yahoo.com/quote/F34.SI?p=F34.SI&.tsrc=fin-srch'],
-                'J36.SI':['Jardine Matheson Holdings Limited', 'https://finance.yahoo.com/quote/J36.SI?p=J36.SI&.tsrc=fin-srch'],
-                'YF8.SI':['Yangzijiang Financial Holding Ltd', 'https://finance.yahoo.com/quote/YF8.SI?p=YF8.SI&.tsrc=fin-srch'],
-                'G13.SI':['Genting', 'https://finance.yahoo.com/quote/G13.SI?p=G13.SI&.tsrc=fin-srch'],
-                'AJBU.SI':['Keppel DC REIT', 'https://finance.yahoo.com/quote/AJBU.SI?p=AJBU.SI&.tsrc=fin-srch'],
+                'BN4.SI' : ['Keppel Corporation', 'https://finance.yahoo.com/quote/BN4.SI?p=BN4.SI&.tsrc=fin-srch'],
+                'S63.SI':  ['Singapore Technologies Engineering Ltd', 'https://finance.yahoo.com/quote/S63.SI?p=S63.SI&.tsrc=fin-srch'],
+                'U96.SI':  ['Sembcorp', 'https://finance.yahoo.com/quote/U96.SI?p=U96.SI&.tsrc=fin-srch'],
+                'S68.SI': ['SGX', 'https://finance.yahoo.com/quote/S68.SI?p=S68.SI&.tsrc=fin-srch'],
+                'V03.SI': ['Venture Corp', 'https://finance.yahoo.com/quote/V03.SI?p=V03.SI&.tsrc=fin-srch'],
+                'S58.SI': ['SATS', 'https://finance.yahoo.com/quote/S58.SI?p=S58.SI&.tsrc=fin-srch'],
+                'Y92.SI': ['Thai Beverage', 'https://finance.yahoo.com/quote/Y92.SI?p=Y92.SI&.tsrc=fin-srch'],
+                'C07.SI': ['Jardine Cycle & Carriage', 'https://finance.yahoo.com/quote/C07.SI?p=C07.SI&.tsrc=fin-srch'],
+                'C09.SI': ['City Developments', 'https://finance.yahoo.com/quote/C09.SI?p=C09.SI&.tsrc=fin-srch'],
+                'N2IU.SI': ['Mapletree Pan Asia Commercial Trust', 'https://finance.yahoo.com/quote/N2IU.SI?p=N2IU.SI&.tsrc=fin-srch'],
+                'H78.SI': ['Hong Kong Land holdings', 'https://finance.yahoo.com/quote/H78.SI?p=H78.SI&.tsrc=fin-srch'],
+                'F34.SI': ['Wilmar International', 'https://finance.yahoo.com/quote/F34.SI?p=F34.SI&.tsrc=fin-srch'],
+                'J36.SI': ['Jardine Matheson Holdings Limited', 'https://finance.yahoo.com/quote/J36.SI?p=J36.SI&.tsrc=fin-srch'],
+                'YF8.SI': ['Yangzijiang Financial Holding Ltd', 'https://finance.yahoo.com/quote/YF8.SI?p=YF8.SI&.tsrc=fin-srch'],
+                'G13.SI': ['Genting', 'https://finance.yahoo.com/quote/G13.SI?p=G13.SI&.tsrc=fin-srch'],
+                'AJBU.SI': ['Keppel DC REIT', 'https://finance.yahoo.com/quote/AJBU.SI?p=AJBU.SI&.tsrc=fin-srch'],
                 'BS6.SI': ['Yangzijiang' 'https://finance.yahoo.com/quote/BS6.SI?p=BS6.SI&.tsrc=fin-srch']
                  }
 
@@ -40,7 +39,7 @@ def write_file(buy_arg,filepath):
     with open(filepath,'a') as f:
         f.writelines(buy_arg + ' ')
 
-#write records
+# write records
 def get_list(filepath):
     with open(filepath, 'r') as f:
         buy = f.readlines()
@@ -59,18 +58,18 @@ def bollinger():
 
         stock_info = yf.Ticker(stockkey)
         stock_data = stock_info.history
-        twentyMA = (round(stock_info.history(period="20d", interval="1d").mean(), 2)['Close'])
+        twentyma = (round(stock_info.history(period="20d", interval="1d").mean(), 2)['Close'])
         stddev = stock_info.history(period="20d", interval="1d")['Close'].std(ddof=0)
-        topband = twentyMA + 2 * stddev
-        lowerband = twentyMA - 2 * stddev
+        topband = twentyma + 2 * stddev
+        lowerband = twentyma - 2 * stddev
 
         if Closing > topband:
-            selltext = (f"\n {stockkey} - {STI_dict[stockkey][0]} [chart]({STI_dict[stockkey][1]})")
+            selltext = f"\n {stockkey} - {STI_dict[stockkey][0]} [chart]({STI_dict[stockkey][1]})"
             sell2 = write_file(selltext,  filepath='currentsell.txt')
 
 
         if Closing < lowerband:
-            buytext = (f"\n {stockkey} - {STI_dict[stockkey][0]} [chart]({STI_dict[stockkey][1]})")
+            buytext = f"\n {stockkey} - {STI_dict[stockkey][0]} [chart]({STI_dict[stockkey][1]})"
             buy2 = write_file(buytext,filepath='currentbuy.txt')
 
         else:
@@ -110,10 +109,16 @@ def rsical():
         #print(stock, latest_rsi)
 
         if latest_rsi > 70:
-            sell2 = write_file('\n'+ stockkey + " - " + STI_dict[stockkey], filepath='currentsell.txt')
+            selltext = f"""\n {stockkey} - {STI_dict[stockkey][0]} [chart]({STI_dict[stockkey][1]})
+            latest rsi = {latest_rsi}
+            """
+            sell2 = write_file(selltext, filepath='currentsell.txt')
 
         elif latest_rsi < 30:
-            buy2 = write_file('\n'+ stockkey + " - " + STI_dict[stockkey], filepath='currentbuy.txt')
+            buytext = f"""\n {stockkey} - {STI_dict[stockkey][0]} [chart]({STI_dict[stockkey][1]})
+            RSI = {latest_rsi}
+            """
+            buy2 = write_file(buytext, filepath='currentbuy.txt')
 
 
         else:
@@ -132,10 +137,12 @@ def MA():
         twentyMA = round(stock_info.history(period="20d", interval="1d").mean(), 2)
 
         if (twentyMA['Close'])> (fiftyMA['Close']):
-            buy2 = write_file('\n'+ stockkey + " - " + STI_dict[stockkey], filepath='currentbuy.txt')
+            buytext = (f"\n {stockkey} - {STI_dict[stockkey][0]} [chart]({STI_dict[stockkey][1]})")
+            buy2 = write_file(buytext, filepath='currentbuy.txt')
 
         elif (twentyMA['Close']) < (fiftyMA['Close']):
-            sell = write_file('\n'+ stockkey + " - " + STI_dict[stockkey], filepath='currentsell.txt')
+            selltext = (f"\n {stockkey} - {STI_dict[stockkey][0]} [chart]({STI_dict[stockkey][1]})")
+            sell2 = write_file(selltext, filepath='currentsell.txt')
         else:
             pass
 
